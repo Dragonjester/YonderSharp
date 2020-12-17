5c367f95b028c071eacd5d22644899e8a4949881
Deltahedron <kamikaze311@gmx.de>
2020-12-17 23:45:49 +0100

    Missing DataContract

--- YonderSharp.Modell/TimeStampedValue.cs
+++ YonderSharp.Modell/TimeStampedValue.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace YonderSharp.Modell
 {
+    [DataContract]
     public class TimeStampedValue<T>
     {
+        [DataMember]
         public DateTime CreatedOnUTC { get; } = DateTime.Now;
+        [DataMember]
         public DateTime LastChange { get; private set; } = DateTime.Now;
+        [DataMember]
         public T Value { get; private set; }
+
         public void UpdateValue(T newValue)
         {
             Value = newValue;
