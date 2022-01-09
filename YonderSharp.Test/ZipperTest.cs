050049a314668a6960f14b34d0dfef0f8f3d83d8
Deltahedron <kamikaze311@gmx.de>
2022-01-09 12:17:12 +0100

    Zipping of objects to files

--- YonderSharp.Test/ZipperTest.cs
+++ YonderSharp.Test/ZipperTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -29,6 +30,51 @@ namespace YonderSharp.Test
 
             var unzipped = Zipper.Unzip(zipped);
             Assert.IsTrue(testValue == unzipped);
+        }
+
+        [Test]
+        public void ZipUnzipToFromFile()
+        {
+            ZipContent original = new ZipContent();
+            original.A = "A";
+            original.B = 6;
+
+            string pathToZip = AppDomain.CurrentDomain.BaseDirectory + "ZipUnzipToFromFile.zip";
+
+            Zipper.Zip(original, pathToZip);
+            ZipContent unziped = Zipper.Unzip<ZipContent>(pathToZip);
+
+            Assert.AreEqual(original, unziped);
+        }
+
+
+
+    }
+
+    [DataContract]
+    public class ZipContent
+    {
+        [DataMember]
+        public string A { get; set; }
+
+        [DataMember]
+        public int B { get; set; }
+
+        [DataMember]
+        public DateTime C { get; set; } = DateTime.UtcNow;
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is ZipContent compareObj))
+            {
+                return false;
+            }
+
+            return A == compareObj.A && B == compareObj.B && C == compareObj.C;
         }
     }
 }
--- YonderSharp/Zipper.cs
+++ YonderSharp/Zipper.cs
@@ -1,20 +1,26 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.IO.Compression;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 
-namespace YonderSharp {
+namespace YonderSharp
+{
     /// <summary>
     /// Helper class for zipping related operations
     /// </summary>
-    public class Zipper {
-        private static void CopyTo(Stream src, Stream dest) {
+    public class Zipper
+    {
+        private static void CopyTo(Stream src, Stream dest)
+        {
             var bytes = new byte[4096];
 
             int cnt;
 
-            while((cnt = src.Read(bytes, 0, bytes.Length)) != 0) {
+            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+            {
                 dest.Write(bytes, 0, cnt);
             }
         }
@@ -22,7 +28,8 @@ namespace YonderSharp {
         /// <summary>
         /// returns the compressed base64 byte[] representation of the content
         /// </summary>
-        public static string ZipBase64(string sr) {
+        public static string ZipBase64(string sr)
+        {
             var data = Zip(sr);
             return Convert.ToBase64String(data);
         }
@@ -30,12 +37,16 @@ namespace YonderSharp {
         /// <summary>
         /// compresses the content
         /// </summary>
-        public static byte[] Zip(string str) {
+        public static byte[] Zip(string str)
+        {
             var bytes = Encoding.UTF8.GetBytes(str);
 
-            using(var msi = new MemoryStream(bytes)) {
-                using(var mso = new MemoryStream()) {
-                    using(var gs = new GZipStream(mso, CompressionLevel.Optimal)) {
+            using (var msi = new MemoryStream(bytes))
+            {
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(mso, CompressionLevel.Optimal))
+                    {
                         CopyTo(msi, gs);
                     }
 
@@ -44,13 +55,64 @@ namespace YonderSharp {
             }
         }
 
+
+        /// <summary>
+        /// Turns the object into a JSON, zips it and stores it into a file
+        /// </summary>
+        public static void Zip(object obj, string outputPath)
+        {
+            if(obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            if (string.IsNullOrEmpty(outputPath))
+            {
+                throw new ArgumentNullException(nameof(outputPath));
+            }
+
+            Zip(JsonConvert.SerializeObject(obj), outputPath);
+        }
+
+        /// <summary>
+        /// Zips the string and stores it into a file
+        /// </summary>
+        public static void Zip(string content, string outputPath)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            byte[] resultToSave = Zip(content);
+            File.WriteAllBytes(outputPath, resultToSave);
+        }
+
+        /// <summary>
+        /// Unzips a file created by the Zip(object, path) method
+        /// </summary>
+        public static T Unzip<T>(string outputPath) 
+        {
+            if (string.IsNullOrEmpty(outputPath))
+            {
+                throw new ArgumentNullException(nameof(outputPath));
+            }
+
+            string json = Unzip(File.ReadAllBytes(outputPath));
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
         /// <summary>
         /// Transformes the zipped bytes to their content string
         /// </summary>
-        public static string Unzip(byte[] bytes) {
-            using(var msi = new MemoryStream(bytes)) {
-                using(var mso = new MemoryStream()) {
-                    using(var gs = new GZipStream(msi, CompressionMode.Decompress)) {
+        public static string Unzip(byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+            {
+                using (var mso = new MemoryStream())
+                {
+                    using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                    {
                         CopyTo(gs, mso);
                     }
 
