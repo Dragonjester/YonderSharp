b1a5eebdd68f11b6b3161a246f002098a93f9390
André Schulz <kamikaze311@gmx.de>
2023-07-12 15:09:36 +0200

    Start of CopyProtection

---
+++ YonderSharp/CopyProtection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace YonderSharp
+{
+    /// <summary>
+    /// The Goal of this isn't to create 100% security, just enough so that stuff can't spread to the wild forever
+    /// </summary>
+    public class CopyProtection
+    {
+        public static bool IsDeveloperMachine()
+        {
+            return Environment.MachineName.Equals("DESKTOP-VBKF9PE");
+        }
+
+    }
+}
