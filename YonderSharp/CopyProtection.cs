e235f2f277470fb63b1396cc96bbc4be6b8308ac
André Schulz <kamikaze311@gmx.de>
2023-11-25 10:49:35 +0100

    generic IsDeveloperMachine

--- YonderSharp/CopyProtection.cs
+++ YonderSharp/CopyProtection.cs
@@ -13,6 +13,21 @@ namespace YonderSharp
         private static string[] developerIds = new[] { "0781607453387f55" , "DESKTOP-VBKF9PE", "be6abc1efa0463e7" };
         private static bool hasDeveloperMachineRun;
         private static bool isDeveloperMachine;
+
+        public static bool IsDeveloperMachine()
+        {
+            if (hasDeveloperMachineRun)
+            {
+                return isDeveloperMachine;
+            }
+
+
+            var name = System.Net.Dns.GetHostName();
+
+            isDeveloperMachine = developerIds.Contains(name);
+            hasDeveloperMachineRun = true;
+            return isDeveloperMachine;
+        }
         
         public static bool IsDeveloperMachine(IDevice device)
         {
