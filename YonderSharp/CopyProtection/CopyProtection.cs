06ceabae18fae6814e54416af530dab491d4a3f3
André Schulz <kamikaze311@gmx.de>
2024-04-03 17:21:02 +0200

    Add date function

--- YonderSharp/CopyProtection/CopyProtection.cs
+++ YonderSharp/CopyProtection/CopyProtection.cs
@@ -3,16 +3,32 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net.NetworkInformation;
 
-namespace YonderSharp
+namespace YonderSharp.CopyProtection
 {
     /// <summary>
-    /// The Goal of this isn't to create 100% security, just enough so that stuff can't spread to the wild forever
+    /// The Goal of this isn't to create 100% security. The goal is to prohibit that {stuff} can spread to the wild uncontrolled forever
     /// </summary>
-    public class CopyProtection
+    public partial class CopyProtection
     {
-        private static string[] developerIds = new[] { "0781607453387f55" , "DESKTOP-VBKF9PE", "be6abc1efa0463e7", "8e6e644b116b2354" };
+        private static string[] developerIds = new[] { "0781607453387f55", "DESKTOP-VBKF9PE", "be6abc1efa0463e7", "8e6e644b116b2354" };
         private static bool hasDeveloperMachineRun;
         private static bool isDeveloperMachine;
+
+
+        public static DateTime LastAllowedDay()
+        {
+            return GetCompileDate().AddDays(365);
+        }
+
+        public static bool IsAllowedToRun()
+        {
+            return IsDeveloperMachine() || DateTime.Now <= LastAllowedDay();
+        }
+
+        public static bool IsAllowedToRun(IDevice device)
+        {
+            return IsDeveloperMachine(device) || DateTime.Now <= LastAllowedDay();
+        }
 
         public static bool IsDeveloperMachine()
         {
@@ -28,7 +44,7 @@ namespace YonderSharp
             hasDeveloperMachineRun = true;
             return isDeveloperMachine;
         }
-        
+
         public static bool IsDeveloperMachine(IDevice device)
         {
             if (device == null)
