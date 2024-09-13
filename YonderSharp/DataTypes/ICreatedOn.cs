6db31521aa37fad97b6fd4573ac5969d7ca50056
André Schulz <kamikaze311@gmx.de>
2024-09-13 18:05:18 +0200

    simple interface

---
+++ YonderSharp/DataTypes/ICreatedOn.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace YonderSharp.DataTypes
+{
+    public interface ICreatedOn
+    {
+        public DateTime CreatedOn { get; set; }
+
+        public string CreatedBy { get; set; }
+    }
+}
