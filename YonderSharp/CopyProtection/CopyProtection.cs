ea036443ff3da1c4b226da814050d02ed854175f
Dragonjester <kamikaze311@gmx.de>
2025-03-01 16:30:39 +0100

    upgrade to .net 9

--- YonderSharp/CopyProtection/CopyProtection.cs
+++ YonderSharp/CopyProtection/CopyProtection.cs
@@ -9,7 +9,7 @@ namespace YonderSharp.CopyProtection
     /// </summary>
     public partial class CopyProtection
     {
-        private static string[] developerIds = new[] { "0781607453387f55", "DESKTOP-VBKF9PE", "be6abc1efa0463e7", "8e6e644b116b2354" };
+        private static string[] developerIds = new[] { "0781607453387f55", "DESKTOP-VBKF9PE", "be6abc1efa0463e7", "8e6e644b116b2354" , "CAA-LEGION" };
         private static bool hasDeveloperMachineRun;
         private static bool isDeveloperMachine;
 
