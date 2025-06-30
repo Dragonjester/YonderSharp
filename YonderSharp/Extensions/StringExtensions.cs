50733202d2cf72525a7338ca5e3addd2ab53d78e
André Schulz <kamikaze311@gmx.de>
2025-06-30 20:21:07 +0200

    forgotten change

--- YonderSharp/Extensions/StringExtensions.cs
+++ YonderSharp/Extensions/StringExtensions.cs
@@ -1,7 +1,17 @@
-﻿namespace YonderSharp.Extensions
+﻿using System;
+
+namespace YonderSharp.Extensions
 {
     public static class StringExtensions
     {
         public static bool HasContent(this string str) => !string.IsNullOrEmpty(str);
+
+        public static string FirstCharToUpper(this string input) =>
+        input switch
+        {
+            null => throw new ArgumentNullException(nameof(input)),
+            "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+            _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
+        };
     }
 }
